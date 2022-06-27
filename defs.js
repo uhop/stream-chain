f7e401566b3ea3f1d9cb5d4393dc058f3679437a
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2022-06-26 21:45:05 -0500

    Added missing definitions (not all).

--- defs.js
+++ defs.js
@@ -3,6 +3,7 @@
 const none = Symbol.for('object-stream.none');
 const finalSymbol = Symbol.for('object-stream.final');
 const manySymbol = Symbol.for('object-stream.many');
+const stop = Symbol.for('object-stream.stop');
 
 const finalValue = value => ({[finalSymbol]: value});
 const many = values => ({[manySymbol]: values});
@@ -13,9 +14,10 @@ const isMany = o => o && typeof o == 'object' && manySymbol in o;
 const getFinalValue = o => o[finalSymbol];
 const getManyValues = o => o[manySymbol];
 
-class Stop {}
+class Stop extends Error {}
 
 module.exports.none = none;
+module.exports.stop = stop;
 module.exports.finalValue = finalValue;
 module.exports.isFinalValue = isFinalValue;
 module.exports.getFinalValue = getFinalValue;
