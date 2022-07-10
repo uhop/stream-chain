7ea30083504f73e8d6698e65f47e5d5a811750fb
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2022-07-09 19:21:18 -0500

    Added utilities to fix UTF-8 streams and read a stream by lines.

---
+++ src/utils/lines.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const {none, flushable} = require('../defs');
+
+const lines = () => {
+  let rest = '';
+  return flushable(function* (value) {
+    if (value === none) {
+      if (!rest) return;
+      const result = rest;
+      rest = '';
+      yield result;
+      return;
+    }
+    const lines = value.split('\n');
+    rest += lines[0];
+    if (lines.length < 2) return;
+    lines[0] = rest;
+    rest = lines.pop();
+    yield* lines;
+  });
+};
+
+module.exports = lines;
