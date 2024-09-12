bf42edbb64e6b0f4026ee23e8927ac4df7511f89
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-09-12 18:35:52 -0500

    Added typings + tests for `fun`.

--- src/fun.d.ts
+++ src/fun.d.ts
@@ -1,10 +1,11 @@
+import type {Many, Flatten} from './defs';
+import type {Arg0, Ret, FnList} from './gen';
+
 export = fun;
 
-export type Fn = (chunk: unknown, encoding?: string) => unknown;
-export type Iter = Iterable<unknown> | AsyncIterable<unknown>;
-
-type FunType<Self> = Fn | Iter | null | undefined | Self[];
-
-interface FunArg extends FunType<FunArg> {}
-
-declare function fun(...fns: FunArg[]): Promise<unknown>;
+declare function fun(): (arg: any) => Many<any> | any;
+declare function fun<L extends unknown[]>(
+  ...fns: FnList<Arg0<L>, L>
+): Flatten<L> extends readonly [function, ...function[]]
+  ? (arg: Arg0<L>) => Many<Ret<L>> | Ret<L>
+  : (arg: any) => Many<any> | any;
---
+++ ts-check/fun.ts
@@ -0,0 +1,48 @@
+import fun from 'stream-chain/fun.js';
+
+const f0 = fun();
+const f1 = fun((x: number) => x * x);
+const f2 = fun(
+  (x: number) => x * x,
+  (x: number) => String(x).split(' ')
+);
+const f3 = fun(
+  (x: number) => x * x,
+  [f0, f1, f2] as const,
+  (x: string[]) => x[0],
+  (x: string) => !x.split(' '),
+  (x: boolean) => !x
+);
+const f4 = fun([
+  (x: number) => x * x,
+  [f0, f1, f2],
+  (x: string[]) => x[0],
+  (x: string) => !x.split(' '),
+  (x: boolean) => !x
+] as const);
+const f5 = fun(
+  async (x: number) => x * x,
+  function* (x: number) {
+    yield* [x - 1, x, x + 1];
+  },
+  async function* (x: number) {
+    for (let i = x; i > 0; --i) {
+      yield i;
+    }
+  },
+  (x: any) => String(x)
+);
+
+const fns: ((arg: any) => any)[] = [f3, (x: boolean) => Number(x), f4];
+const f6 = fun(...fns);
+const f7 = fun(
+  function* () {
+    for (let i = 0; i < 10; ++i) {
+      yield i;
+    }
+  },
+  f6
+);
+
+void f5;
+void f7;
