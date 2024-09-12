f29a87c6ec78e6845791a25be998f9993c9dabce
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-09-11 22:33:47 -0500

    More realistic typings for `gen()`.

--- src/gen.d.ts
+++ src/gen.d.ts
@@ -1,10 +1,58 @@
-export = gen;
+export = Gen.gen;
 
-export type Fn = (chunk: any, encoding?: string) => unknown;
-export type Iter = Iterable<unknown> | AsyncIterable<unknown>;
+declare namespace Gen {
+  type First<L extends readonly unknown[]> = L extends readonly [infer T, ...readonly unknown[]] ? T : never;
+  type Last<L extends readonly unknown[]> = L extends readonly [...readonly unknown[], infer T] ? T : never;
+  type Flatten<L extends readonly unknown[]> = L extends readonly [infer T, ...infer R]
+    ? T extends readonly unknown[]
+      ? readonly [...Flatten<T>, ...Flatten<R>]
+      : readonly [T, ...Flatten<R>]
+    : L;
 
-type GenType<Self> = Fn | Iter | null | undefined | Self[];
+  type Fn<I = any, O = any> = (arg: I, ...rest: readonly unknown[]) => O;
 
-interface GenArg extends GenType<GenArg> {}
+  type Arg0<F> = F extends readonly unknown[]
+    ? Flatten<F> extends readonly [infer F1, ...readonly unknown[]]
+      ? Arg0<F1>
+      : Flatten<F> extends readonly (infer F1)[]
+      ? Arg0<F1>
+      : never
+    : F extends (...args: readonly any[]) => unknown
+    ? Parameters<F>[0]
+    : never;
 
-declare function gen(...fns: GenArg[]): AsyncIterableIterator<unknown>;
+  type Ret<F> = F extends readonly unknown[]
+    ? Flatten<F> extends readonly [...unknown[], infer F1]
+      ? Ret<F1>
+      : never
+    : F extends Fn
+    ? ReturnType<F> extends Generator<infer O, unknown, unknown>
+      ? O
+      : ReturnType<F> extends AsyncGenerator<infer O, unknown, unknown>
+      ? O
+      : ReturnType<F> extends Promise<infer O>
+      ? O
+      : ReturnType<F>
+    : never;
+
+  type FnItem<I, F> = F extends readonly [infer F1, ...infer R]
+    ? readonly [FnItem<I, F1>, ...FnList<Ret<F1>, R>]
+    : F extends Fn<any, unknown>[]
+    ? readonly [FnItem<I, F[number]>]
+    : F extends Fn<any, unknown>
+    ? I extends Arg0<F>
+      ? F
+      : (arg: I, ...rest: unknown[]) => ReturnType<F>
+    : never;
+
+  type FnList<I, L> = L extends readonly [infer F1, ...infer R] ? readonly [FnItem<I, F1>, ...FnList<Ret<F1>, R>] : L;
+
+  function gen(): (arg: any) => AsyncGenerator<any, void, unknown>;
+  function gen<L extends unknown[]>(
+    ...fns: FnList<Arg0<L>, L>
+  ): Flatten<L> extends readonly [Fn, ...Fn[]]
+    ? (arg: Arg0<L>) => AsyncGenerator<Ret<L>, void, unknown>
+    : Flatten<L> extends Fn[]
+    ? (arg: any) => AsyncGenerator<any, void, unknown>
+    : never;
+}
---
+++ ts-check/gen.ts
@@ -0,0 +1,48 @@
+import gen from 'stream-chain/gen.js';
+
+const g0 = gen();
+const g1 = gen((x: number) => x * x);
+const g2 = gen(
+  (x: number) => x * x,
+  (x: number) => String(x).split(' ')
+);
+const g3 = gen(
+  (x: number) => x * x,
+  [g0, g1, g2] as const,
+  (x: string[]) => x[0],
+  (x: string) => !x.split(' '),
+  (x: boolean) => !x
+);
+const g4 = gen([
+  (x: number) => x * x,
+  [g0, g1, g2],
+  (x: string[]) => x[0],
+  (x: string) => !x.split(' '),
+  (x: boolean) => !x
+] as const);
+const g5 = gen(
+  async (x: number) => x * x,
+  function* (x: number) {
+    yield* [x - 1, x, x + 1];
+  },
+  async function* (x: number) {
+    for (let i = x; i > 0; --i) {
+      yield i;
+    }
+  },
+  (x: any) => String(x)
+);
+
+const fns: ((arg: any) => any)[] = [g3, (x: boolean) => Number(x), g4];
+const g6 = gen(...fns);
+const g7 = gen(
+  function* () {
+    for (let i = 0; i < 10; ++i) {
+      yield i;
+    }
+  },
+  g6
+);
+
+void g5;
+void g7;
