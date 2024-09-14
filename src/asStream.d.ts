eaa22f0935e1cbd0461639b9a351fb9bf71800c8
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-09-14 00:29:56 -0500

    Removed `TypedDuplex`.

--- src/asStream.d.ts
+++ src/asStream.d.ts
@@ -1,13 +1,10 @@
 /// <reference types="node" />
 
 import {Duplex, DuplexOptions} from 'node:stream';
-import type {OutputType} from './defs';
 
 export = asStream;
-
-export interface TypedDuplex<I, O> extends Duplex {}
 
 declare function asStream<F extends (chunk: any, encoding?: string) => unknown>(
   fn: F,
   options?: DuplexOptions
-): TypedDuplex<Parameters<F>[0], OutputType<F>>;
+): Duplex;
