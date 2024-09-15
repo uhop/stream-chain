e6c5a7716e4067ba9f3603e1603d2d4895e742e1
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-09-14 23:14:05 -0500

    Converted to a regular TS file.

--- src/typed-streams.ts
+++ src/typed-streams.ts
@@ -3,20 +3,19 @@
 import {Duplex, Readable, Transform, Writable} from 'node:stream';
 
 export class TypedDuplex<W = any, R = W> extends Duplex {
-  __streamTypeR(): R;
-  __streamTypeW(): W;
+  __streamTypeR(): R { return null as R; }
+  __streamTypeW(): W { return null as W; }
 }
 
 export class TypedReadable<R = any> extends Readable {
-  __streamTypeR(): R;
+  __streamTypeR(): R { return null as R; }
 }
 
 export class TypedTransform<W = any, R = W> extends Transform {
-  __streamTypeR(): R;
-  __streamTypeW(): W;
+  __streamTypeR(): R { return null as R; }
+  __streamTypeW(): W { return null as W; }
 }
 
 export class TypedWritable<W = any> extends Writable {
-  __streamTypeW(): W;
+  __streamTypeW(): W { return null as W; }
 }
-
